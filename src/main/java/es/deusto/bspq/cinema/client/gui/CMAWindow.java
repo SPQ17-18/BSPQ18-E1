9837e4e0de7c29681b9bf7c4188f9dd05cd16534
Íñigo García <inigogc@opendeusto.es>
2018-04-11 19:20:20 +0200

    First design of CMAWindow

--- src/main/java/es/deusto/bspq/cinema/client/gui/CMAWindow.java
+++ src/main/java/es/deusto/bspq/cinema/client/gui/CMAWindow.java
@@ -7,14 +7,141 @@ import java.rmi.RemoteException;
 import javax.swing.JFrame;
 
 import es.deusto.bspq.cinema.client.controller.CMController;
+import java.awt.BorderLayout;
+import javax.swing.JComboBox;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class CMAWindow extends JFrame {
 
+	private String[] foo = {"Item 1", "Item 2", "Item 3", "Item 4"};
+	
 	private static final long serialVersionUID = 1L;
 	private CMController controller;
+	private JPanel panelCentral = new JPanel();
+	private JPanel panelInsert = new JPanel();
+	private JTabbedPane tabbedPaneInsert = new JTabbedPane(JTabbedPane.TOP);
+	private JPanel panelInsertSession = new JPanel();
+	private JButton btnInsertSession = new JButton("Insert");
+	private JPanel panelInsertFilm = new JPanel();
+	private JButton btnInsertFilm = new JButton("Insert");
+	private JPanel panelUpdate = new JPanel();
+	private JTabbedPane tabbedPaneUpdate = new JTabbedPane(JTabbedPane.TOP);
+	private JPanel panelUpdateSession = new JPanel();
+	private JComboBox comboBoxUpdateSession = new JComboBox(foo);
+	private JButton btnUpdateSession = new JButton("Update");
+	private JPanel panelUpdateFilm = new JPanel();
+	private JComboBox comboBoxUpdateFilm = new JComboBox(foo);
+	private JButton btnUpdateFilm = new JButton("Update");
+	private JPanel panelDelete = new JPanel();
+	private JTabbedPane tabbedPaneDelete = new JTabbedPane(JTabbedPane.TOP);
+	private JPanel panelDeleteSession = new JPanel();
+	private JComboBox comboBoxDeleteSession = new JComboBox(foo);
+	private JButton btnDeleteSession = new JButton("Delete");
+	private JPanel panelDeleteFilm = new JPanel();
+	private JComboBox comboBoxDeleteFilm = new JComboBox(foo);
+	private JButton btnDeleteFilm = new JButton("Delete");
 	
 	public CMAWindow(CMController controller) {
+		setResizable(false);
+		setTitle("CMAWindow");
 		this.controller = controller;
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		getContentPane().add(panelCentral, BorderLayout.CENTER);
+		panelCentral.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		panelInsert.setBorder(new TitledBorder(null, "Insert", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCentral.add(panelInsert);
+		panelCentral.setMaximumSize(new java.awt.Dimension(1200, 120));
+		
+		panelInsert.add(tabbedPaneInsert);
+		
+		tabbedPaneInsert.addTab("Session", null, panelInsertSession, null);
+		btnInsertSession.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				//Go to InsertSessionWindow
+			}
+		});
+		
+		panelInsertSession.add(btnInsertSession);
+		panelInsertSession.setPreferredSize(new java.awt.Dimension(320, 100));
+		
+		tabbedPaneInsert.addTab("Film", null, panelInsertFilm, null);
+		btnInsertFilm.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				//Go to InsertFilmWindow
+			}
+		});
+		
+		panelInsertFilm.add(btnInsertFilm);
+		
+		panelUpdate.setBorder(new TitledBorder(null, "Update", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCentral.add(panelUpdate);
+		
+		panelUpdate.add(tabbedPaneUpdate);
+		
+		tabbedPaneUpdate.addTab("Session", null, panelUpdateSession, null);
+		
+		panelUpdateSession.add(comboBoxUpdateSession);
+		btnUpdateSession.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				//Go to UpdateSessionWindow
+			}
+		});
+		
+		panelUpdateSession.add(btnUpdateSession);
+		panelUpdateSession.setPreferredSize(new java.awt.Dimension(320, 100));
+		
+		tabbedPaneUpdate.addTab("Film", null, panelUpdateFilm, null);
+		
+		panelUpdateFilm.add(comboBoxUpdateFilm);
+		btnUpdateFilm.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				//Go to UpdateFilmWindow
+			}
+		});
+		
+		panelUpdateFilm.add(btnUpdateFilm);
+		
+		panelDelete.setBorder(new TitledBorder(null, "Delete", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelCentral.add(panelDelete);
+		
+		panelDelete.add(tabbedPaneDelete);
+		
+		tabbedPaneDelete.addTab("Session", null, panelDeleteSession, null);
+		
+		panelDeleteSession.add(comboBoxDeleteSession);
+		btnDeleteSession.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				//Delete Session
+			}
+		});
+		
+		panelDeleteSession.add(btnDeleteSession);
+		panelDeleteSession.setPreferredSize(new java.awt.Dimension(320, 100));
+		
+		tabbedPaneDelete.addTab("Film", null, panelDeleteFilm, null);
+		
+		panelDeleteFilm.add(comboBoxDeleteFilm);
+		btnDeleteFilm.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				//Delete Film
+			}
+		});
+		
+		panelDeleteFilm.add(btnDeleteFilm);
 		initComponents();
 	}
 
@@ -28,7 +155,7 @@ public class CMAWindow extends JFrame {
 		setLocation((dim.width - abounds.width) / 2, (dim.height - abounds.height) / 2);
 	}
 
-	/** Exit the Application */
+	// Exit the Application 
 	private void exitForm(java.awt.event.WindowEvent evt) {
 		controller.exit();
 	}
