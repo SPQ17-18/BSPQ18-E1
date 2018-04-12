455c3768e25e5f9b6f650a5ece6fe656160701c7
Ander Arguiñano <ander.arguinano@opendeusto.es>
2018-04-12 23:30:17 +0200

    Started loading the db #B18E1-28 add work 1h Testing

--- src/main/java/es/deusto/bspq/cinema/server/jdo/DAO/ManagerDAO.java
+++ src/main/java/es/deusto/bspq/cinema/server/jdo/DAO/ManagerDAO.java
@@ -13,9 +13,11 @@ import javax.jdo.Transaction;
 
 import org.datanucleus.api.jdo.JDOQuery;
 
+import es.deusto.bspq.cinema.server.jdo.data.Actor;
 import es.deusto.bspq.cinema.server.jdo.data.Employee;
 import es.deusto.bspq.cinema.server.jdo.data.Film;
 import es.deusto.bspq.cinema.server.jdo.data.Member;
+import es.deusto.bspq.cinema.server.jdo.data.Room;
 import es.deusto.bspq.cinema.server.jdo.data.Session;
 
 
@@ -161,23 +163,6 @@ private PersistenceManagerFactory pmf;
 	    return film;
 	}
 	
-	
-	
-	public static void main(String[] args) {
-		IManagerDAO dao= new ManagerDAO();
-		
-		if (args.length != 3) {
-			System.out.println("Attention: arguments missing");
-			System.exit(0);
-		}
-
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
-		
-		
-	}
-
 	
 	public void storeFilm(Film film) {
 		System.out.println("   * Storing a film: " + film.getTitle());
@@ -718,4 +703,98 @@ private PersistenceManagerFactory pmf;
 	    return session;
 	}
 
+	public static void main(String[] args) {
+		IManagerDAO dao= new ManagerDAO();
+		
+		if (args.length != 3) {
+			System.out.println("Attention: arguments missing");
+			System.exit(0);
+		}
+
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+		
+		Film f1 = new Film("Inmersion", "Wim Wenders", 12, 111, "EE.UU.");
+		Film f2 = new Film("Pacific Rim: Insurrecion", "Steven S. DeKnight", 7, 110, "EE.UU.");
+		Film f3 = new Film("Leo Da Vinci", "Sergio Manfio", 0, 85, "Italia");
+		Film f4 = new Film("Campeones", "Javier Fesser", 7, 100, "España");
+		Film f5 = new Film("Ready Player One", "Steven Spielberg", 7, 140, "EE.UU.");
+		Film f6 = new Film("Un pliegue en el tiempo", "Ava DuVernay", 7, 109, "EE.UU.");
+		Film f7 = new Film("Tomb Raider", "Roar Uthaug", 7, 122, "EE.UU.");
+		
+		Actor a1 = new Actor("Reese", "Witherspoon", "EE.UU.");
+		Actor a2 = new Actor("Chris", "Pine", "EE.UU.");
+		
+		f6.addActor(a1);
+		f6.addActor(a2);
+		
+		Actor a3 = new Actor("Alicia", "Vikander", "EE.UU.");
+		Actor a4 = new Actor("Walton", "Goggins", "EE.UU.");
+		
+		f7.addActor(a3);
+		f7.addActor(a4);
+		
+	
+		Actor a5 = new Actor("Tye", "Sheridan", "EE.UU.");
+		Actor a6 = new Actor("Olivia", "Cooke", "EE.UU.");
+		
+		f5.addActor(a5);
+		f5.addActor(a6);
+		
+		
+		Actor a7 = new Actor("Javier", "Gutiérrez", "España");
+		Actor a8 = new Actor("Juan", "Margallo", "España");
+		
+		f4.addActor(a7);
+		f4.addActor(a8);
+		
+		Actor a9 = new Actor("Juan", "Garcia", "Italia");
+		Actor aA = new Actor("Paolo", "Linares", "Italia");
+		
+		f3.addActor(a9);
+		f3.addActor(aA);
+		
+		Actor aB = new Actor("Scott", "Eastwood", "EE.UU.");
+		Actor aC = new Actor("John", "Boyega", "EE.UU.");
+		
+		f2.addActor(aB);
+		f2.addActor(aC);
+		
+		Actor aD = new Actor("James", "McAvoy", "EE.UU.");
+		Actor aE = new Actor("Alicia", "Vikander", "EE.UU.");
+		
+		f1.addActor(aD);
+		f1.addActor(aE);
+		
+		Employee e1 = new Employee("e1", "Juan", "Garcia Perez", "e1", 1500);
+		Employee e2 = new Employee("e2", "Maria", "Martin Gomez", "e2", 1700);
+		Employee e3 = new Employee("e3", "Paco", "Perez Gomez", "e3", 1300);
+		Employee e4 = new Employee("e4", "Luis", "Lozano Esteban", "e4", 1800);
+		Employee e5 = new Employee("e5", "Andrea", "Hernandez Sarria", "e5", 1500);
+		
+		
+		Room r1 = new Room(1, 58);
+		Room r2 = new Room(2, 56);
+		Room r3 = new Room(3, 57);
+		Room r4 = new Room(4, 59);
+		Room r5 = new Room(5, 60);
+		
+		dao.storeFilm(f1);
+		dao.storeFilm(f2);
+		dao.storeFilm(f3);
+		dao.storeFilm(f4);
+		dao.storeFilm(f5);
+		dao.storeFilm(f6);
+		dao.storeFilm(f7);
+		
+		dao.storeEmployee(e1);
+		dao.storeEmployee(e2);
+		dao.storeEmployee(e3);
+		dao.storeEmployee(e4);
+		dao.storeEmployee(e5);
+		
+		
+		
+	}
 }
